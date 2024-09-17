134193c4cae8e69e52468514f30c26eff9fb5507
Nisarg2061 <khodkenisarg@gmail.com>
2024-09-18 00:49:48 +0530

    Update nvim

--- nvim/nvim/lua/autocommands/autosave.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd("BufWritePost", {
-  group = vim.api.nvim_create_augroup("AutoRun", {clear = true}),
-  pattern = {"main.go", "example.go"},
-  callback = function ()
-    vim.cmd("! make run")
-  end,
-})
