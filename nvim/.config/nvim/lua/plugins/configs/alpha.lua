85fcf2771ce66995105cebd9fad457677aac97ff
Nisarg <khodkenisarg@gmail.com>
2025-02-19 12:08:47 +0530

    upd

--- nvim/.config/nvim/lua/plugins/configs/alpha.lua
+++ nvim/.config/nvim/lua/plugins/configs/alpha.lua
@@ -1,17 +1,28 @@
 local dashboard = require("alpha.themes.dashboard")
 
+dashboard.section.header.val = {
+  [[ ███╗   ██╗██╗███████╗ █████╗ ██████╗  ██████╗  ]],
+  [[ ████╗  ██║██║██╔════╝██╔══██╗██╔══██╗██╔════╝  ]],
+  [[ ██╔██╗ ██║██║███████╗███████║██████╔╝██║  ███╗ ]],
+  [[ ██║╚██╗██║██║╚════██║██╔══██║██╔══██╗██║   ██║ ]],
+  [[ ██║ ╚████║██║███████║██║  ██║██║  ██║╚██████╔╝ ]],
+  [[ ╚═╝  ╚═══╝╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝  ]],
+
+
+}
+
 dashboard.section.buttons.val = {
-    dashboard.button("f", "  Find file", "<cmd>Telescope find_files<cr>"),
-    dashboard.button("n", "  New file", "<cmd>ene | startinsert<cr>"),
-    dashboard.button("r", "  Recent files", "<cmd>Telescope oldfiles<cr>"),
-    dashboard.button("c", "  Config", "<cmd>e $MYVIMRC<cr>"),
-    dashboard.button("s", "  Restore Session", "<cmd>lua require('persistence').load()<cr>"),
-    dashboard.button("q", "  Quit", "<cmd>qa<cr>")
+  dashboard.button("f", "  Find file", "<cmd>Telescope find_files<cr>"),
+  dashboard.button("n", "  New file", "<cmd>ene | startinsert<cr>"),
+  dashboard.button("r", "  Recent files", "<cmd>Telescope oldfiles<cr>"),
+  dashboard.button("c", "  Config", "<cmd>e $MYVIMRC<cr>"),
+  dashboard.button("s", "  Restore Session", "<cmd>lua require('persistence').load()<cr>"),
+  dashboard.button("q", "  Quit", "<cmd>qa<cr>")
 }
 
 for _, button in ipairs(dashboard.section.buttons.val) do
-    button.opts.hl = "AlphaButtons"
-    button.opts.hl_shortcut = "AlphaShortcut"
+  button.opts.hl = "AlphaButtons"
+  button.opts.hl_shortcut = "AlphaShortcut"
 end
 
 dashboard.section.header.opts.hl = "AlphaHeader"
