829e9495c5c460372f10d1329c5e6ea5e7d99a6b
dracuxan <khodkenisarg@gmail.com>
2025-03-03 01:30:14 +0530

    upd

--- install.sh
+++ install.sh
@@ -8,32 +8,7 @@ echo "
 ╚═════╝  ╚═════╝    ╚═╝   ╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝
                                                              "
 
-set -e  # Exit if any command fails
-
-# Detect package manager
-if command -v apt &> /dev/null; then
-    PKG_MANAGER="apt"
-elif command -v pacman &> /dev/null; then
-    PKG_MANAGER="pacman"
-else
-    echo "Unsupported package manager. Please install manually."
-    exit 1
-fi
-
-# Define required packages
-PACKAGES=( "fastfetch")
-
-# Install missing packages
-echo "Checking for required packages..."
-for pkg in "${PACKAGES[@]}"; do
-    if [ "$PKG_MANAGER" = "apt" ]; then
-        sudo apt install -y "$pkg"
-    elif [ "$PKG_MANAGER" = "pacman" ]; then
-        sudo pacman -S --noconfirm "$pkg"
-    fi
-done
-
-echo "Installation complete using $PKG_MANAGER!"
+set -e # Exit if any command fails
 
 # Get the directory of the script
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -47,5 +22,6 @@ mkdir -p "$HOME/.config/fastfetch"
 echo "Stowing dotfiles..."
 stow -d "$DOTFILES_DIR" -t "$HOME/.config/nvim" nvim
 stow -d "$DOTFILES_DIR" -t "$HOME/.config/fastfetch" fastfetch
+stow -d "$DOTFILES_DIR" -t "$HOME/.config" starship
 
 echo "Done. Enjoy."
