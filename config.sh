00676f22b36ae4f5ddd542a0bce6a1ef527f75f7
dracuxan <khodkenisarg@gmail.com>
2025-04-06 03:14:54 +0530

    revert dir struct

--- config.sh
+++ config.sh
@@ -9,8 +9,19 @@ sudo apt install -y stow
 echo "[+] Creating .config if missing..."
 mkdir -p "$HOME/.config"
 
+# Ensure ~/.config exists
+mkdir -p "$HOME/.config"
+mkdir -p "$HOME/.config/nvim"
+mkdir -p "$HOME/.config/fastfetch"
+mkdir -p "$HOME/.config/alacritty"
+
+
+# Use Stow to symlink dotfiles to ~/.config
 echo "[+] Syncing .config files..."
-rsync -av --progress "$DOTFILES_DIR/.config/" "$HOME/.config/"
+stow -d "$DOTFILES_DIR" -t "$HOME/.config/nvim" nvim
+stow -d "$DOTFILES_DIR" -t "$HOME/.config/alacritty" alacritty
+stow -d "$DOTFILES_DIR" -t "$HOME/.config/fastfetch" fastfetch
+stow -d "$DOTFILES_DIR" -t "$HOME/.config" starship
 
 echo "[+] Stowing legacy dotfiles..."
 stow -d "$DOTFILES_DIR" -t "$HOME" tmux
@@ -18,9 +29,8 @@ stow -d "$DOTFILES_DIR" -t "$HOME" zsh
 
 if ! grep -q 'LOGO_DIR' ~/.zshrc; then
     echo '[+] Adding LOGO_DIR to .zshrc'
-    echo 'export LOGO_DIR="$HOME/.config/fastfetch/logos"' >> ~/.zshrc
+    echo 'export LOGO_DIR="$HOME/.config/fastfetch/logos"' >>~/.zshrc
 fi
 
 echo "[+] Done. Reloading zsh..."
 exec zsh
-
