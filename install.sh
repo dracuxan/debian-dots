ee97277fe5027c5efdbedce44e943cb6d766eec7
dracuxan <khodkenisarg@gmail.com>
2025-05-01 01:27:46 +0530

    rm setup scripts
    not reliable

--- install.sh
+++ install.sh
@@ -9,16 +9,8 @@ echo "-----------------------------------------"
 echo "[+] Updating system..."
 sudo apt update && sudo apt upgrade -y
 
-echo "[+] Installing essentials..."
-sudo apt install -y curl git unzip tar stow
-
-echo "[+] Running setup scripts..."
-
-chmod +x setup/*.sh
-
-./setup/00-essentials.sh
-# ./setup/02-dwm.sh
-./setup/99-final-touches.sh
+echo "[+] Installing stow..."
+sudo apt install -y stow
 
 echo "[+] Stowing dotfiles..."
 chmod +x config.sh
